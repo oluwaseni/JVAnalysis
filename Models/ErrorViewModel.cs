af4ab2698312a2ebe228dde0aac2d36459da99f9
Oladunni Folarin <foladunni@oandoenergyresources.com>
2021-07-22 14:18:03 +0100

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JV_Analysis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
